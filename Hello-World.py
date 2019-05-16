f007792e958d6c00c3efac9e738bb066309ca05f
YektaAkhalili <50711176+YektaAkhalili@users.noreply.github.com>
2019-05-16 20:35:12 +0430

    Python code for hello world
    With a Twist!

---
+++ Hello-World.py
@@ -0,0 +1,4 @@
+#just a silly code to put on Github for starters!
+#written May  16th, 2019
+n = input("Hey there, What's your name?")
+print("I wanted to say Hello, World! But, you're not World! You're {}!".format(n))
